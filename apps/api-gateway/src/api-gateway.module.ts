d68e37051a920610a4815e3f65b719675e2025d2
Rithy Bondeth <rithybondeth999@gmail.com>
2025-02-05 01:13:36 +0700

    setup health check

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -5,7 +5,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { UploadfileModule } from '@app/common/uploadfile/uploadfile.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { HealthController } from './health/health.controller';
 import * as path from 'path';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
@@ -13,13 +15,13 @@ import * as path from 'path';
       isGlobal: true,
       envFilePath: './apps/api-gateway/.env',
     }), 
-    LoggerModule, AuthModule, UserModule, UploadfileModule,
+    LoggerModule, AuthModule, UserModule, UploadfileModule, TerminusModule,
     ServeStaticModule.forRoot({
       rootPath: path.join(process.cwd(), 'storage'),
       serveRoot: '/storage',
     })
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class ApiGatewayModule {}
