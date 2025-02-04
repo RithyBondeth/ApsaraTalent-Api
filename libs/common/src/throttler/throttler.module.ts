b55777d3cf52e2b3ff93bc095ed6395a71f79c71
Rithy Bondeth <rithybondeth999@gmail.com>
2025-02-05 01:01:32 +0700

    setup rate limiting throttler library

--- libs/common/src/throttler/throttler.module.ts
+++ libs/common/src/throttler/throttler.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule as NestThrottlerModule } from '@nestjs/throttler';
+import { throttlerConfig } from './config/throttler.config';
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: './libs/.env',
+        }),
+        NestThrottlerModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: throttlerConfig,
+        })
+    ],
+    exports: [NestThrottlerModule]
+})
 export class ThrottlerModule {}
