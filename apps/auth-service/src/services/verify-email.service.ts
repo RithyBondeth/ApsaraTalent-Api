aed0c522aba83bcfa30f70a1595db6cb9f91cb96
Rithy Bondeth <rithybondeth999@gmail.com>
2025-02-04 23:56:32 +0700

    improve httpcode in each routes and handle verifyemail logic

--- apps/auth-service/src/services/verify-email.service.ts
+++ apps/auth-service/src/services/verify-email.service.ts
@@ -1,9 +1,42 @@
 import { User } from "@app/common/database/entities/user.entity";
-import { Injectable } from "@nestjs/common";
+import { JwtService } from "@app/common/jwt/jwt.service";
+import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { PinoLogger } from "nestjs-pino";
 import { Repository } from "typeorm";
+import { VerifyEmailResponseDTO } from "../dtos/verify-email-response.dto";
 
 @Injectable()
 export class VerifyEmailService {
-    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>,
+        private readonly jwtService: JwtService,
+        private readonly logger: PinoLogger,
+    ) {}
+
+    async verifyEmail(emailVerificationToken: string): Promise<VerifyEmailResponseDTO> {
+        try {
+            //Find the user by email verification token
+            const decoded = await this.jwtService.verifyEmailToken(emailVerificationToken);
+            const user = await this.userRepository.findOne({ 
+                where: { 
+                    emailVerificationToken: emailVerificationToken,
+                    email: decoded.email,
+                } 
+            });
+            if(!user) throw new UnauthorizedException('Invalid Credentials');
+
+            //Set email verification to true and email verification token to null
+            user.isEmailVerified = true;
+            user.emailVerificationToken = null;
+
+            //Save user into the database 
+            await this.userRepository.save(user);
+
+            return new VerifyEmailResponseDTO('Your email was verified successfully. Now you can login');
+        } catch (error) {
+            this.logger.error(error.message);
+            throw new BadRequestException('An error occurred while verifying email');
+        }
+    }  
 }
