c2be55ac4c6b39cc0e3d8d9a49ea3e364e2eddf2
Rithy Bondeth <rithybondeth999@gmail.com>
2025-05-12 01:22:05 +0700

    udpdate signup response

--- libs/common/src/uploadfile/uploadfiles.interceptor.ts
+++ libs/common/src/uploadfile/uploadfiles.interceptor.ts
@@ -1,7 +1,6 @@
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { UploadfileService } from './uploadfile.service';
 import { mixin, Type } from '@nestjs/common';  // <-- important!
-import { diskStorage } from 'multer';
 
 export function UploadFilesInterceptor(fieldName: string, folderName: string, maxCount = 10): Type<any> {
     class MixinUploadFilesInterceptor extends FilesInterceptor(fieldName, maxCount, {
