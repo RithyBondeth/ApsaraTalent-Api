78b1e87dc6e24bf949e5f2c5f45fd734bf17e6bd
Rithy Bondeth <rithybondeth999@gmail.com>
2025-06-06 00:04:37 +0700

    implement chat-service

--- apps/chat-service/src/chat-service.module.ts
+++ apps/chat-service/src/chat-service.module.ts
@@ -5,6 +5,8 @@ import { DatabaseModule, LoggerModule } from '@app/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@app/common/database/entities/user.entity';
 import { Chat } from '@app/common/database/entities/chat.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { USER_SERVICE } from 'utils/constants/user-service.constant';
 
 @Module({
   imports: [
@@ -14,7 +16,17 @@ import { Chat } from '@app/common/database/entities/chat.entity';
     }),
     LoggerModule,
     DatabaseModule,
-    TypeOrmModule.forFeature([ User, Chat ])
+    TypeOrmModule.forFeature([ User, Chat ]),
+    ClientsModule.register([
+      {
+        name: USER_SERVICE.NAME,
+        transport: Transport.TCP,
+        options: {
+          host: process.env.USER_SERVICE_HOST,
+          port: parseInt(process.env.USER_SERVICE_PORT),
+        },
+      },
+    ]),
   ],
   controllers: [],
   providers: [ChatServiceService],
--- libs/common/src/interfaces/chat.interface.ts
+++ libs/common/src/interfaces/chat.interface.ts
@@ -1,12 +1,30 @@
-export type TChatPayload = {
+export interface TChatPayload {
   receiverId: string;
   content: string;
   type?: 'text' | 'image' | 'file';
-};
+}
 
-export type TChatContent = {
+export interface TChatContent extends TChatPayload {
+  senderId: string;
+  timestamp?: Date;
+}
+
+export interface IChatMessage {
+  id: string;
   senderId: string;
   receiverId: string;
   content: string;
-  type: string;
-};
+  messageType: string;
+  isRead: boolean;
+  sendAt: Date;
+  sender?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  receiver?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
