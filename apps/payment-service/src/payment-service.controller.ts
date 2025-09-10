e7b9cbdbb461b8d1a19bf2901bb40616f9b7c567
Rithy Bondeth <rithybondeth999@gmail.com>
2025-09-10 10:07:09 +0700

    setup payment-service controller

--- apps/payment-service/src/payment-service.controller.ts
+++ apps/payment-service/src/payment-service.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { PaymentServiceService } from './payment-service.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { PAYMENT_SERVICE } from 'utils/constants/payment-service.constant';
+import { GenerateIndividualKhqrDTO } from './dtos/generate-individual-khqr.dto';
+import { GenerateMerchantKhqrDTO } from './dtos/generate-merchant-khqr.dto';
 
 @Controller()
 export class PaymentServiceController {
   constructor(private readonly paymentServiceService: PaymentServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentServiceService.getHello();
+  @MessagePattern(PAYMENT_SERVICE.ACTIONS.GENERATE_INDIVIDUAL_KHQR)
+  async generateIndividualKHQR(
+    @Payload() generateIndividualKhqrDTO: GenerateIndividualKhqrDTO,
+  ): Promise<any> {
+    return this.paymentServiceService.generateIndividualKhqrDTO(
+      generateIndividualKhqrDTO,
+    );
+  }
+
+  @MessagePattern(PAYMENT_SERVICE.ACTIONS.GENERATE_MERCHANT_KHQR)
+  async generateMerchantKhqrDTO(
+    @Payload() generateMerchantKhqrDTO: GenerateMerchantKhqrDTO,
+  ): Promise<any> {
+    return this.paymentServiceService.generateMerchantKhqrDTO(
+      generateMerchantKhqrDTO,
+    );
   }
 }
