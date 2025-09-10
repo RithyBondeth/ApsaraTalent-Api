e1bbe6ecbc412fddf74d8f2fde49af762277e1ef
Rithy Bondeth <rithybondeth999@gmail.com>
2025-09-10 10:07:09 +0700

    setup payment-service controller

--- apps/payment-service/src/payment-service.service.ts
+++ apps/payment-service/src/payment-service.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { GenerateIndividualKhqrDTO } from './dtos/generate-individual-khqr.dto';
+import { GenerateMerchantKhqrDTO } from './dtos/generate-merchant-khqr.dto';
 
 @Injectable()
 export class PaymentServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  async generateIndividualKhqrDTO(generateIndividualKhqrDTO: GenerateIndividualKhqrDTO): Promise<any> {}
+
+  async generateMerchantKhqrDTO(generateMerchantKhqrDTO: GenerateMerchantKhqrDTO): Promise<any> {}
 }
