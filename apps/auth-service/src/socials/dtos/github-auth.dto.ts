27e623ef7a89cfed619faa23a26c1859b2130236
Rithy Bondeth <rithybondeth999@gmail.com>
2025-10-17 23:22:49 +0700

    update remove-one-image route

--- apps/auth-service/src/socials/dtos/github-auth.dto.ts
+++ apps/auth-service/src/socials/dtos/github-auth.dto.ts
@@ -1,22 +1,22 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsOptional, IsString } from 'class-validator';
 
 export class GithubAuthDTO {
-    @IsString()
-    @IsOptional()
-    id: string;
+  @IsString()
+  @IsOptional()
+  id: string;
 
-    @IsString()
-    @IsOptional()
-    username: string;
+  @IsString()
+  @IsOptional()
+  username: string;
 
-    @IsString()
-    @IsOptional()
-    email: string;
+  @IsString()
+  @IsOptional()
+  email: string;
 
-    @IsString()
-    @IsOptional()
-    picture: string;
+  @IsString()
+  @IsOptional()
+  picture: string;
 
-    @IsString()
-    provider: string;
+  @IsString()
+  provider: string;
 }
