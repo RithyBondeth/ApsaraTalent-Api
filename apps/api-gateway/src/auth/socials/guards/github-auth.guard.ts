e5661e49a031da31e29385b4a6d8b5956c26dbbe
Rithy Bondeth <rithybondeth999@gmail.com>
2025-12-11 10:09:59 +0700

    fixed linkedin and github remember auth

--- apps/api-gateway/src/auth/socials/guards/github-auth.guard.ts
+++ apps/api-gateway/src/auth/socials/guards/github-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GithubAuthGuard extends AuthGuard('github') {}
+export class GithubAuthGuard extends AuthGuard('github') {
+  canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest();
+
+    // Save remember query
+    if (req.query.remember)
+      req.session.remember = req.query.remember === 'true';
+
+    return super.canActivate(context);
+  }
+
+  // Allow OAuth to continue
+  handleRequest = (err: any, user: any) => user;
+}
