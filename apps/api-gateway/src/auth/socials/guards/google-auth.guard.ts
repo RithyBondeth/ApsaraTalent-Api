5be90e9f8dce56adf23a1b2df8d825fa3a2d11ba
Rithy Bondeth <rithybondeth999@gmail.com>
2025-12-11 09:56:17 +0700

    fixed google auth remember me

--- apps/api-gateway/src/auth/socials/guards/google-auth.guard.ts
+++ apps/api-gateway/src/auth/socials/guards/google-auth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') {}
+export class GoogleAuthGuard extends AuthGuard('google') {
+    canActivate(context: ExecutionContext) {
+        const req = context.switchToHttp().getRequest();
+    
+        // Save remember query
+        if (req.query.remember) {
+          req.session.remember = req.query.remember === 'true';
+        }
+    
+        return super.canActivate(context);
+      }
+    
+      // Allow OAuth to continue
+      handleRequest(err, user) {
+        return user;
+      }
+}
